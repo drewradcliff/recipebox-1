3bb1e5226622f7bc76245c67d69cb538369d7616
Chris Warren <chris@chriswarren.dev>
2020-08-06 18:36:12 -0400

    Updated several files

--- recipe/views.py
+++ recipe/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from recipe.models import Author, Recipe
 
+
 # Create your views here.
 def index(request):
     my_recipes = Recipe.objects.all()
     return render(request, "index.html", {"recipe_box": "My Recipe Box", "recipes": my_recipes})
 
 
-def  author_detail(request, author_id):
+def author_detail(request, author_id):
     the_author = Author.objects.filter(id=author_id).first()
     recipe_contributed = Recipe.objects.filter(author=the_author)
     return render(request, "author_detail.html", {"author": the_author, "recipes": recipe_contributed})
